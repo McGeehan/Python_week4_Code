4e6f466e7cd4ec0ff61c7a5677a8e3f53fb2756a
MkGee <matthewrmcgeehan2@gmail.com>
2023-10-25 10:45:50 -0400

    Update Mood(incomplete).py

--- Mood(incomplete).py
+++ Mood(incomplete).py
@@ -4,11 +4,11 @@
 print("Hello, world!")
 print("Hello, what is your name?")
 myName = input()
-print("it's good to meet you," + myName)
+print("It's good to meet you," + myName)
 
-# asking how the person is doing
-print("tell me, how has your day been going on a scale of one to five")
-input()
+# Asking how the person is doing
+print("Tell me, how has your day been going on a scale of one to five")
+day_rating = int(input())
 
 # Values
 terrible = 1
@@ -17,20 +17,18 @@ okay = 3
 good = 4
 great = 5
 
-
 # Responses
 def getAnswer(getNumber):  # answers vary with number
     if getNumber == 1:  # lowest Value
-        print  ('I am sorry to hear that. hopefully things get better.')
+        print('I am sorry to hear that. Hopefully things get better.')
     elif getNumber == 2:
-        print('I am sorry to hear that. hopefully things get better.')
+        print('That sounds like a rough day. I hope it gets better.')
     elif getNumber == 3:
-        print  ('I am sorry to hear that. hopefully things get better.')
+        print('I am glad to hear it has been okay.')
     elif getNumber == 4:
-        print('I am sorry to hear that. hopefully things get better.')
+        print('That sounds like a good day!')
     elif getNumber == 5:  # highest Value
-        print('I am sorry to hear that. hopefully things get better.')
+        print('Wow! I am glad to hear it has been great.')
 
-
-# Way of getting responses 1-5
-input(getAnswer)
+# Get responses 1-5
+getAnswer(day_rating)
